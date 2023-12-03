cb58c6f03ca910e36c77f68b9158b9223e6120b5
nbe <dongmingli_Ben@126.com>
2023-12-03 19:48:41 +0000

    working template

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,48 @@
 module Main where
 
-import Brick (Widget, joinBorders, simpleMain, str, withBorderStyle, (<+>))
-import Brick.Widgets.Border (borderWithLabel, vBorder)
-import Brick.Widgets.Border.Style (unicode)
-import Brick.Widgets.Center (center)
+import Brick
+  ( App (..),
+    attrMap,
+    customMain,
+    neverShowCursor,
+  )
+import Brick.BChan (newBChan)
+import qualified Graphics.Vty as V
+import Types
+import UI
 
-ui :: Widget ()
-ui =
-  joinBorders $
-    withBorderStyle unicode $
-      borderWithLabel (str "Hello!") $
-        (center (str "Left") <+> vBorder <+> center (str "Right"))
+app :: App Game Tick Name
+app =
+  App
+    { appDraw = drawUI,
+      appChooseCursor = neverShowCursor,
+      appHandleEvent = handleEvent,
+      appStartEvent = return,
+      appAttrMap = const $ attrMap V.defAttr []
+    }
+
+initialState :: Game
+initialState =
+  Game
+    { x = 0,
+      y = 0,
+      sheild = 100,
+      sword = 100,
+      hp = 100,
+      attack = 100
+    }
 
 main :: IO ()
-main = simpleMain ui
+main =
+  do
+    eventChan <- Brick.BChan.newBChan 10
+    let buildVty = V.mkVty V.defaultConfig
+    initialVty <- buildVty
+    finalState <-
+      customMain
+        initialVty
+        buildVty
+        (Just eventChan)
+        app
+        initialState
+    return ()
---
+++ src/Types.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Types where
+
+-- Types
+
+-- | Game state
+data Game = Game
+  { x :: Int,
+    y :: Int,
+    sheild :: Int,
+    sword :: Int,
+    hp :: Int,
+    attack :: Int
+  }
+  deriving (Show)
+
+-- Types
+
+-- | Ticks mark passing of time
+--
+-- This is our custom event that will be constantly fed into the app.
+data Tick = Tick
+
+-- | Named resources
+--
+-- Not currently used, but will be easier to refactor
+-- if we call this "Name" now.
+type Name = ()
