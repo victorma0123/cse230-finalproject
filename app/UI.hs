5d3c8ec0b5142a6db6580d5fe1a0eafa1a3eb111
Fan Tang <clarencetangfan33@gmail.com>
2023-12-08 19:18:15 +0000

    Expand the map to full box, and add Event box and exit program feature.

--- app/UI.hs
+++ app/UI.hs
@@ -30,6 +30,7 @@ handleEvent g (VtyEvent (V.EvKey (V.KChar 'w') [])) = continue $ g { posY = posY
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'a') [])) = continue $ g { posX = posX g - 1 }
 handleEvent g (VtyEvent (V.EvKey (V.KChar 's') [])) = continue $ g { posY = posY g + 1 }
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'd') [])) = continue $ g { posX = posX g + 1 }
+handleEvent g (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt g
 handleEvent g _ = continue g
 -- Drawing
 
@@ -38,20 +39,20 @@ fixedSizeWidget width height = hLimit width $ vLimit height $ fill ' '
 
 
 -- string
-stringposition g = str $ "X Position: " ++ show (posX g) ++ "  Y Position: " ++ show (posY g)
+stringposition g = str $ "Press q to exit\n" ++ "X Position: " ++ show (posX g) ++ "  Y Position: " ++ show (posY g)
 
 drawUI :: Game -> [Widget Name]
 drawUI g =
-  let mapRows = [ createRow y g | y <- [0..3] ]  -- 生成地图的每一行
+  let mapRows = [ createRow y g | y <- [0..10] ]  -- 生成地图的每一行
   in [ border $ vBox
-        [ fixedSizeWidget 62 12, stringposition g
+        [stringposition g
         , vBox mapRows  -- 将地图行添加到界面中
         , border $ hBox [ border $ vBox 
-                    [ fixedSizeWidget 30 3,
-                      fixedSizeWidget 30 3 , str $ "sheild: \n" ++ "sword:\n" ++ "hp:\n" ++ "attack:\n"]
+                    [ str $ "sheild:\n" ++ "sword:\n" ++ "hp:\n" ++ "attack:\n",
+                      fixedSizeWidget 30 1 ]
                 , border $ vBox 
-                    [fixedSizeWidget 30 3,
-                      fixedSizeWidget 30 3]
+                    [ str $ "Event:\n" ++ "Choice 1:\n" ++ "Choice 2: \n",
+                      fixedSizeWidget 30 2]
                 ]
         ]
      ]
@@ -59,11 +60,11 @@ drawUI g =
 -- 创建地图的一行
 createRow :: Int -> Game -> Widget Name
 createRow y g =
-  let mapCells = [ createCell x y g | x <- [0..3] ]  -- 生成一行中的每个格子
+  let mapCells = [ createCell x y g | x <- [0..30] ]  -- 生成一行中的每个格子
   in hBox mapCells
 
 -- 创建单个格子
 createCell :: Int -> Int -> Game -> Widget Name
 createCell x y g =
-  let content = if x == posX g && y == posY g then "玩家" else "  "  -- 根据游戏状态决定格子内容
+  let content = if x == posX g && y == posY g then "Player" else "  "  -- 根据游戏状态决定格子内容
   in str content
