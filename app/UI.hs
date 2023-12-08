ecebd3009f62131b76959bddfdfa37be901f8eaa
victorma0123 <victor.ma.liangchun@gmail.com>
2023-12-08 04:43:27 +0000

    update on movement, need more

--- app/UI.hs
+++ app/UI.hs
@@ -36,16 +36,34 @@ handleEvent g _ = continue g
 fixedSizeWidget :: Int -> Int -> Widget n
 fixedSizeWidget width height = hLimit width $ vLimit height $ fill ' '
 
+
+-- string
+stringposition g = str $ "X Position: " ++ show (posX g) ++ "  Y Position: " ++ show (posY g)
+
 drawUI :: Game -> [Widget Name]
 drawUI g =
-  [ border $ vBox
-      [ fixedSizeWidget 62 12, str $ "X Position: " ++ show (posX g) ++ "Y Position: " ++ show (posY g)
-      , border $ hBox [ border $ vBox 
-                  [ fixedSizeWidget 30 3,
-                     fixedSizeWidget 30 3 , str $ "sheild: \n" ++ "sword:"]
-              , border $ vBox 
-                  [fixedSizeWidget 30 3,
-                     fixedSizeWidget 30 3]
-              ]
-      ]
-  ]
+  let mapRows = [ createRow y g | y <- [0..3] ]  -- 生成地图的每一行
+  in [ border $ vBox
+        [ fixedSizeWidget 62 12, stringposition g
+        , vBox mapRows  -- 将地图行添加到界面中
+        , border $ hBox [ border $ vBox 
+                    [ fixedSizeWidget 30 3,
+                      fixedSizeWidget 30 3 , str $ "sheild: \n" ++ "sword:\n" ++ "hp:\n" ++ "attack:\n"]
+                , border $ vBox 
+                    [fixedSizeWidget 30 3,
+                      fixedSizeWidget 30 3]
+                ]
+        ]
+     ]
+
+-- 创建地图的一行
+createRow :: Int -> Game -> Widget Name
+createRow y g =
+  let mapCells = [ createCell x y g | x <- [0..3] ]  -- 生成一行中的每个格子
+  in hBox mapCells
+
+-- 创建单个格子
+createCell :: Int -> Int -> Game -> Widget Name
+createCell x y g =
+  let content = if x == posX g && y == posY g then "玩家" else "  "  -- 根据游戏状态决定格子内容
+  in str content
